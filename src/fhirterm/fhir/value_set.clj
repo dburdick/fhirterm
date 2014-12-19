067a85dffaad7339c491f86ec17fb24d0e19ae35
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2014-12-19 13:17:28 +0300

    refactor VS$expand

--- src/fhirterm/fhir/value_set.clj
+++ src/fhirterm/fhir/value_set.clj
@@ -21,7 +21,7 @@
       (into (vector (map (fn [cs] {:property "code" :value cs :op "in"})
                          (map (fn [i] (map :code (:concept i))) inc))))))
 
-(defn- expand-with-includes [db vs]
+(defn- expand-with-compose-include [expansion db vs]
   (let [includes (get-in vs [:compose :include])
         ns-and-filters (reduce (fn [acc [s fs]]
                                  (assoc acc s (filter (complement nil?)
@@ -30,9 +30,9 @@
 
     (reduce (fn [res [ns filters]]
               (into res (naming-system/filter-codes db ns filters)))
-            [] ns-and-filters)))
+            expansion ns-and-filters)))
 
-(defn- expand-with-defines [db {{:keys [system concept]} :define :as vs}]
+(defn- expand-with-define [expansion db {{:keys [system concept]} :define :as vs}]
   (reduce (fn reduce-fn [result c]
             (let [result (conj result
                                {:code    (:code c)
@@ -42,10 +42,10 @@
               (if inner-concept
                 (into result (reduce reduce-fn [] inner-concept))
                 result)))
-          []
+          expansion
           concept))
 
-(defn- apply-coding-filters [codings params]
+(defn- apply-expansion-filters [codings params]
   (let [filter-str (:filter params)]
     (if (and filter-str (not (str/blank? filter-str)))
       ;; perform filtering
@@ -58,18 +58,20 @@
       ;; otherwise, just return all codings
       codings)))
 
-(defn- expand* [db vs]
-  (cond
-   (contains? vs :define) (expand-with-defines db vs)
-   (contains? vs :include) (expand-with-includes db vs)
-   :else (throw (RuntimeException. (format "Don't know how to expand VS %s"
-                                           (:identifier vs))))))
+(defn- expand-with-compose-exclude [expansion db vs]
+  ;; todo!!
+  expansion)
+
+(defn- expand* [db vs params]
+  (-> []
+      (expand-with-define db vs)
+      (expand-with-compose-include db vs)
+      (expand-with-compose-exclude db vs)
+      (apply-expansion-filters params)))
 
 (defn expand [db vs params]
-  (let [result (expand* db vs)
-        filtered-result (apply-coding-filters result params)]
-
+  (let [result (expand* db vs params)]
     (assoc vs :expansion {:identifier (util/uuid)
                           :timestamp (time/now)
                           :contains (map (fn [x] (dissoc x :search-vector))
-                                         filtered-result)})))
+                                         result)})))
